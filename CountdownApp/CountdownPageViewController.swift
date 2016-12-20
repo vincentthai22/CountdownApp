0c6817a103faf61a74b3197a6751f28cb8b9e917
Vincent <vincent@Alexs-MacBook-Pro-2.local>
2016-12-20 13:16:57 -0800

    first commit

---
+++ CountdownApp/CountdownPageViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CountdownApp
+//
+//  Created by Vincent on 12/20/16.
+//  Copyright © 2016 Seven Logics. All rights reserved.
+//
+//  Current Task : create a countdown app that stores an image, has a floating label that shows how many days until the event.  Swipe left and right to view different countdowns and up and down to change pictures. top toolbar has an add button and settings button.
+
+import UIKit
+
+class CountdownPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
